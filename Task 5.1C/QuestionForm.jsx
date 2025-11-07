2169aa943ef182a13ef9eb4870d0b2a5c0f3f2bb
Parampreet Singh <parambansrow@gmail.com>
2025-11-07 22:45:36 +0530

    Add files via upload

---
+++ Task 5.1C/QuestionForm.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Post from "./Post";
+
+function QuestionForm({postType}){
+    return(
+        <form className="ques">
+            <div className="heading">What do you want to ask or share</div>
+            <div id="content">
+                <p>This section is designed based on the type of the post. It could be developed by conditional rendering.</p>
+                <p id="red"> For post a {postType}, the following section would be appeared.</p>
+            </div>
+            <div id="text">
+                <p>Title</p>
+                <input className="textBox" type="text" placeholder="Start your question with how, what, why, etc." required/>
+            </div>
+            
+            <div>
+                <p>Describe your problem</p>
+                <textarea id="des" type="text" required minLength={5} maxLength={100}/>
+            </div>
+
+            <div id="tag">
+                <p>Tags</p>
+                <input className="textBox" type="text" placeholder="Please add up to 3 tags to describe your question e.g., Java" required/>
+            </div>
+
+            <Post/>
+            
+        </form>
+    )
+}
+
+export default QuestionForm;
